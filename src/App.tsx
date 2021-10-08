c643b9310eb009c68c676a350d0f92fa6a204106
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-10-08 12:41:33 -0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
+//pages
 import { Home } from "./pages/Home";
 
 export default function App() {
