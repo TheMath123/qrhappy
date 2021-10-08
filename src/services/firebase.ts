24b681b65ba03d9262effb3034d0c5b6fa23ec0a
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-10-08 11:21:45 -0300

    install sucecss firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
-import { }
+import database from 'firebase/database';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = firebase.database()
