532cd1139441642322052c1aa0e1f216c3e366e3
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-10-08 11:37:43 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import database from 'firebase/database';
+import { getAuth } from "firebase/auth";
+import { database } from "firebase/database";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +22,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const database = firebase.database()
+const database = firebase.database();
+export const auth = getAuth();
