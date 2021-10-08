5d65176cbb9bbba6c437e40028f576aaeb589f91
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-10-08 11:53:32 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,28 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { database } from "firebase/database";
+import firebase from 'firebase/app';
 
+import 'firebase/auth';
+import 'firebase/database';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_IR,
+  appId: process.env.REACT_APP_API_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
 const database = firebase.database();
-export const auth = getAuth();
+
+export { firebase, auth, database }
