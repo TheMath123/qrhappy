4d91c17bd09c90d3c837c841aacf26a024d36b8a
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-10-20 20:37:30 -0300

    add styles module login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,11 @@
+import styles from '../styles/Login.module.scss'
+
 export function Login(){
   return(
     <>
+    <div className={styles.container}>
+
+    </div>
       <h3>Future login screen...</h3>
     </>
   )
