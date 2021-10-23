7a81be047e4a0640e9b79f7480bf109fecb9ca62
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-10-23 13:02:34 -0300

    add provider yahoo

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -7,8 +7,10 @@ export function SignIn() {
   const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
+
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
     signInSuccessUrl: '/home',
+
     // We will display Google and Facebook as auth providers.
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
@@ -18,8 +20,9 @@ export function SignIn() {
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       'microsoft.com',
-
+      'yahoo.com'
     ],
+
     callbacks: {
       // Avoid redirects after sign-in.
       signInSuccessWithAuthResult: () => true,
